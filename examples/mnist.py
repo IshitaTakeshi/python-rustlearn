bdef8bef0241b9a6ac79eb71ca651b4944b26988
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-06 19:45:42 +0900

    Use sklearn's new APIs

--- examples/mnist.py
+++ examples/mnist.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sklearn.cross_validation import KFold
+from sklearn.model_selection import KFold
 from sklearn.datasets import load_digits
 from sklearn.metrics import accuracy_score
 
@@ -21,10 +21,12 @@ def run_example():
 
     data, target = _get_data()
 
-    n_folds = 5
+    n_splits = 5
     accuracy = 0.0
 
-    for (train_idx, test_idx) in KFold(n=len(data), n_folds=n_folds, shuffle=True):
+    kf = KFold(n_splits=n_splits, shuffle=True)
+
+    for (train_idx, test_idx) in kf.split(data):
 
         train_X = data[train_idx]
         train_y = target[train_idx]
@@ -39,7 +41,7 @@ def run_example():
 
         accuracy += accuracy_score(predictions, test_y)
 
-    return accuracy / n_folds
+    return accuracy / n_splits
 
 
 accuracy = run_example()
